1b236a2cfe66cb246e0f1503bc3d09ac79506fd0
Xavierxjr <xavierxjr334@gmail.com>
2021-11-26 11:08:23 +1300

    Updates

---
+++ precache-manifest.b0874828ea6fd2ce211f0973be2000c3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b025cf3119eaabd68747911021e051a",
+    "url": "/xavier-ai/index.html"
+  },
+  {
+    "revision": "26e6a65f31b8e626188b",
+    "url": "/xavier-ai/static/css/main.45124dde.chunk.css"
+  },
+  {
+    "revision": "871c3f02b9f4da07b656",
+    "url": "/xavier-ai/static/js/2.6b207303.chunk.js"
+  },
+  {
+    "revision": "10295e4cf22593398dad8b16c89f9483",
+    "url": "/xavier-ai/static/js/2.6b207303.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26e6a65f31b8e626188b",
+    "url": "/xavier-ai/static/js/main.007ce2a9.chunk.js"
+  },
+  {
+    "revision": "4435c3ddcdc6a8e807e7",
+    "url": "/xavier-ai/static/js/runtime-main.773d4bfe.js"
+  },
+  {
+    "revision": "ed2bb5d1c253f31c07431fc04b48ac0e",
+    "url": "/xavier-ai/static/media/10.ed2bb5d1.jpg"
+  },
+  {
+    "revision": "e20281b15a6fcf3ce2e9ffd153b0cba5",
+    "url": "/xavier-ai/static/media/20.e20281b1.jpg"
+  },
+  {
+    "revision": "d01dcf8e13742892c5b5af4b1d5a1c4e",
+    "url": "/xavier-ai/static/media/5.d01dcf8e.jpg"
+  },
+  {
+    "revision": "ba5d76577270e00165828ba2f49dfa34",
+    "url": "/xavier-ai/static/media/50.ba5d7657.jpg"
+  },
+  {
+    "revision": "75d2ed242f8c1214b93b4422c3d2bfa3",
+    "url": "/xavier-ai/static/media/NZH-article.75d2ed24.jpg"
+  },
+  {
+    "revision": "2f06f7fd4e33746d724e85b9aac24801",
+    "url": "/xavier-ai/static/media/anz-logo.2f06f7fd.jpg"
+  },
+  {
+    "revision": "6052bb69e19f52f86abe5585d87e6300",
+    "url": "/xavier-ai/static/media/asb.6052bb69.png"
+  },
+  {
+    "revision": "fe07fde458db3a233c05b6ca876a1d94",
+    "url": "/xavier-ai/static/media/bnz.fe07fde4.jpg"
+  },
+  {
+    "revision": "198668cff21515794d90a596f3dce624",
+    "url": "/xavier-ai/static/media/home.198668cf.jpg"
+  },
+  {
+    "revision": "c2a1d2442079c9924fb172642e12b988",
+    "url": "/xavier-ai/static/media/pig.c2a1d244.jpg"
+  }
+]);
